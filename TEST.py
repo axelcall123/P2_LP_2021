c1900fe8d5611f7d9ead08ef8dd128af8aabb482
axelcall123 <axel15calderon@gmail.com>
2021-03-21 20:45:04 -0600

    fix orden numero
    ordenar los numeros en el arbol, GRAPHIZ TESST/arbol original antes de la modificacion

--- TEST.py
+++ TEST.py
@@ -23,11 +23,11 @@ def numero():#COMO SE MUESTRAN LOS NUMEROS
 def wuamos():
     fila=0
     columna=0
-    T=[
+    Tu=[
         ['BLABLA',fila,columna,'T_RES'],
 
         ['BEBIDA',fila,columna,'T_SEC'],
-        ['BEBIDA 1',fila,columna,'T_ID'],['PAPAYA',fila,columna,'T_NOM'],['10.0',fila,columna,'T_PRE'],['---',fila,columna,'T_DES'],
+        ['BEBIDA 1',fila,columna,'T_ID'],['PAPAYA',fila,columna,'T_NOM'],['50.0',fila,columna,'T_PRE'],['---',fila,columna,'T_DES'],
         ['BEBIDA 2',fila,columna,'T_ID'],['SANDIA',fila,columna,'T_NOM'],['20.0',fila,columna,'T_PRE'],['???',fila,columna,'T_DES'],
 
         ['PASTEL',fila,columna,'T_SEC'],
@@ -38,13 +38,63 @@ def wuamos():
         ['PIZZA 1',fila,columna,'T_ID'],['PIÑA',fila,columna,'T_NOM'],['25.0',fila,columna,'T_PRE'],['##',fila,columna,'T_DES'],
 
         ['HELAODS',fila,columna,'T_SEC'],
-        ['HELAODS 1',fila,columna,'T_ID'],['A',fila,columna,'T_NOM'],['25.0',fila,columna,'T_PRE'],['##',fila,columna,'T_DES'],
-        ['HELAODS 2',fila,columna,'T_ID'],['B',fila,columna,'T_NOM'],['25.0',fila,columna,'T_PRE'],['##',fila,columna,'T_DES'],
-        ['HELAODS 3',fila,columna,'T_ID'],['C',fila,columna,'T_NOM'],['25.0',fila,columna,'T_PRE'],['##',fila,columna,'T_DES'],
-        ['HELAODS 4',fila,columna,'T_ID'],['D',fila,columna,'T_NOM'],['25.0',fila,columna,'T_PRE'],['##',fila,columna,'T_DES'],
-        ['HELAODS 5',fila,columna,'T_ID'],['E',fila,columna,'T_NOM'],['25.0',fila,columna,'T_PRE'],['##',fila,columna,'T_DES']
+        ['HELAODS 1',fila,columna,'T_ID'],['A',fila,columna,'T_NOM'],['50.0',fila,columna,'T_PRE'],['##',fila,columna,'T_DES'],
+        ['HELAODS 2',fila,columna,'T_ID'],['B',fila,columna,'T_NOM'],['7.0',fila,columna,'T_PRE'],['##',fila,columna,'T_DES'],
+        ['HELAODS 3',fila,columna,'T_ID'],['C',fila,columna,'T_NOM'],['5.0',fila,columna,'T_PRE'],['##',fila,columna,'T_DES'],
+        ['HELAODS 4',fila,columna,'T_ID'],['D',fila,columna,'T_NOM'],['30.0',fila,columna,'T_PRE'],['##',fila,columna,'T_DES'],
+        ['HELAODS 5',fila,columna,'T_ID'],['E',fila,columna,'T_NOM'],['1.0',fila,columna,'T_PRE'],['##',fila,columna,'T_DES']
 
         ]
+    cont=0
+    array_sup=[]#[[SECCION],[ID],[NOMBRE],[PRECIO],[DESCRIPCION]]
+    array_ayuda=[]#TEMPORAMENTE LA SECCION
+    array_seccion=[]#ARRAY SECCION
+    for a in range(1,len(Tu)):#CREA LA MATRIZ SUP
+        if Tu[a][3]=='T_SEC':
+            array_ayuda=Tu[a]
+            array_seccion.append(array_ayuda)
+        elif Tu[a][3]=='T_ID' or Tu[a][3]=='T_NOM' or Tu[a][3]=='T_PRE' or Tu[a][3]=='T_DES':
+            cont+=1  
+            if cont%4==0:
+                array_sup.append([array_ayuda,Tu[a-3],Tu[a-2],Tu[a-1],Tu[a]])
+                cont=0
+
+    array_uno=[]#AYUDA1
+    array_dos=[]##AYUDA2
+    for a in range(len(array_sup)):
+        for b in range(len(array_sup)):
+            if b+1<len(array_sup):#PARA MIRAR ANTES DEQUE CREO UN ERROR AL TOMAR LA ULTIMA MATRIZ
+                if float(array_sup[b][3][0])<float(array_sup[b+1][3][0]):
+                    continue
+                else:#SI !5<4 CAMBIA LA POSICION DE LA MATRIZ
+                    array_uno=array_sup[b]
+                    array_dos=array_sup[b+1]
+                    array_sup[b]=array_dos
+                    array_sup[b+1]=array_uno
+    T=[]
+    T.append(Tu[0])#UNE EL NOMBRE
+    for a in array_seccion:#RECREA LA MATRIZ ANTERIOR ORDENADA
+        T.append(a)
+        for b in range(len(array_sup)):
+            if array_sup[b][0][0]==a[0]:
+                T.append(array_sup[b][1])
+                T.append(array_sup[b][2])
+                T.append(array_sup[b][3])
+                T.append(array_sup[b][4])
+            else:
+                continue
+            
+    # for a in array_seccion:
+    #     T.append(a)
+
+    # for b in range(len(array_sup)):
+    #     T.append(array_sup[b][1])
+    #     T.append(array_sup[b][2])
+    #     T.append(array_sup[b][3])
+    #     T.append(array_sup[b][4])
+    for a in T:
+
+        print(a)
     
     array_nod=[]#PARA UNIR LOS NODOS
     nod = graphviz.Digraph(comment='SI')
@@ -85,7 +135,7 @@ def wuamos():
                 #hr
                 cont=0
         
-        #print(T[a][3],'***')
+        #print(T[a][3],'***')'''
 #wuamos()
 def tilde(letra):
     if letra=='á':
@@ -127,26 +177,10 @@ def grap():
     dot.node('F', 'BEBIDA 2')
     dot.node('G', 'POSTRES 1')
     dot.node('H', 'POSTRES 2')
-    dot.edges(['AB', 'AC','AD','BE','BF','DG','DH'])
+    dot.edges(['AB', 'AC','AD','BF','BE','DH','DG'])
     #dot.edge('B', 'L', constraint='false')
     print(dot.source) 
     dot.render('PDF/Hola', view=True)
 
-def xd():
-    fecha=date.today()
-    num="9.5"
-    numDosD=float(num)
-    print(str(fecha)+" "+f"{numDosD:.2f}")
 
-
-
-def asd():
-    dot = graphviz.Digraph(comment='SI')
-    dot.node('A', 'King Arthur')
-    dot.node('B', 'Sir Bedevere the Wise')
-    dot.node('L', 'Sir Lancelot the Brave')
-
-    dot.edges(['AB', 'AL'])
-    dot.render('PDF/Hola', view=True)
-
-asd()
+wuamos()
--- arbol.py
+++ arbol.py
@@ -1,9 +1,51 @@
 import graphviz
-def arbol(T):
+def arbol(Tu):
+    #SE PUDE BORRAR DESDE AQUI XDXD
+    cont=0
+    array_sup=[]#[[SECCION],[ID],[NOMBRE],[PRECIO],[DESCRIPCION]]
+    array_ayuda=[]#TEMPORAMENTE LA SECCION
+    array_seccion=[]#ARRAY SECCION
+    for a in range(2,len(Tu)):#CREA LA MATRIZ SUP
+        if Tu[a][4]=='SECCION':
+            array_ayuda=Tu[a]
+            array_seccion.append(array_ayuda)
+        elif Tu[a][4]=='ID' or Tu[a][4]=='NOMBRE' or Tu[a][4]=='NUMERO' or Tu[a][4]=='DESCRIPCION':
+            cont+=1  
+            if cont%4==0:
+                array_sup.append([array_ayuda,Tu[a-3],Tu[a-2],Tu[a-1],Tu[a]])
+                cont=0
+
+    array_uno=[]#AYUDA1
+    array_dos=[]##AYUDA2
+    for a in range(len(array_sup)):
+        for b in range(len(array_sup)):
+            if b+1<len(array_sup):#PARA MIRAR ANTES DEQUE CREO UN ERROR AL TOMAR LA ULTIMA MATRIZ
+                if float(array_sup[b][3][0])<float(array_sup[b+1][3][0]):
+                    continue
+                else:#SI !5<4 CAMBIA LA POSICION DE LA MATRIZ
+                    array_uno=array_sup[b]
+                    array_dos=array_sup[b+1]
+                    array_sup[b]=array_dos
+                    array_sup[b+1]=array_uno
+    T=[]
+    T.append(Tu[0])#UNE EL restaurante
+    T.append(Tu[1])#UNE EL 'LFP restaurante'
+    for a in array_seccion:#RECREA LA MATRIZ ANTERIOR ORDENADA
+        T.append(a)
+        for b in range(len(array_sup)):
+            if array_sup[b][0][0]==a[0]:
+                T.append(array_sup[b][1])
+                T.append(array_sup[b][2])
+                T.append(array_sup[b][3])
+                T.append(array_sup[b][4])
+            else:
+                continue
+    #------------------------------------------
+
     array_nod=[]#PARA UNIR LOS NODOS
     nod = graphviz.Digraph(comment='SI')
     nod
-    nod.node('A','RESTAURANTE'+'\n'+T[0][0])#TITULO PRINCIPAL
+    nod.node('A','RESTAURANTE'+'\n'+T[1][0])#TITULO PRINCIPAL
     letra='B'#COMIENZA EN B POR A->RESTAURANTE
     cont_nod=0#CUENTA LA CREACION DE UN NUEVO NODO
     cont=0#PARA SI ES MULTIPLO DE 4
